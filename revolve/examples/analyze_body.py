823903f53be93a45509aa11994469d1f88fb3320
Elte Hupkes <eltehupkes@srxp.com>
2015-06-07 22:14:55 +0200

    Changing analyzer to something that only works with a working version of PyGazebo, easier to verify what I have installed

--- revolve/examples/analyze_body.py
+++ revolve/examples/analyze_body.py
@@ -10,9 +10,11 @@ from __future__ import print_function
 import sys
 from sdfbuilder.math import Vector3
 from .generated_sdf import generate_sdf_robot
-from ..gazebo import analyze_body
+from ..gazebo import analyze_body, analysis_coroutine, connect
 import random
 
+import trollius
+from trollius import From
 
 if len(sys.argv) > 1:
     seed = int(sys.argv[1])
@@ -22,26 +24,34 @@ else:
 random.seed(seed)
 print("Seed: %d" % seed, file=sys.stderr)
 
-# Try a maximum of 100 times
-for i in range(100):
-    # Generate a new robot
-    sdf = generate_sdf_robot(plugin_controller="libtolmodelcontrol.so")
+@trollius.coroutine
+def analysis_func():
+    manager = yield From(connect(("127.0.0.1", 11346)))
 
-    # Find out its intersections and bounding box
-    intersections, bbox = analyze_body(sdf)
+    # Try a maximum of 100 times
+    for i in range(100):
+        # Generate a new robot
+        sdf = generate_sdf_robot(plugin_controller="libtolmodelcontrol.so")
 
-    if intersections:
-        print("Invalid model - intersections detected.", file=sys.stderr)
-    else:
-        print("No model intersections detected!", file=sys.stderr)
-        if bbox:
-            # Translate the model in z direction so it won't land
-            # "in" the ground.
-            # TODO Bounding box of body analyzer is incorrect, so we add a little space
-            # I cannot fully solve that by myself so will have to report
-            print("Model bounding box: (%f, %f, %f)" % bbox, file=sys.stderr)
-            model = sdf.elements[0]
-            model.translate(Vector3(0, 0, 0.5 * bbox[2] + 1))
+        # Find out its intersections and bounding box
+        msg_id = "test_%d" % i
+        intersections, bbox = yield From(analysis_coroutine(sdf, msg_id, manager))
 
-        print(str(sdf))
-        break
+        if intersections:
+            print("Invalid model - intersections detected.", file=sys.stderr)
+        else:
+            print("No model intersections detected!", file=sys.stderr)
+            if bbox:
+                # Translate the model in z direction so it won't land
+                # "in" the ground.
+                # TODO Bounding box of body analyzer is incorrect, so we add a little space
+                # I cannot fully solve that by myself so will have to report
+                print("Model bounding box: (%f, %f, %f)" % bbox, file=sys.stderr)
+                model = sdf.elements[0]
+                model.translate(Vector3(0, 0, 0.5 * bbox[2] + 1))
+
+            print(str(sdf))
+            break
+
+loop = trollius.get_event_loop()
+loop.run_until_complete(analysis_func())
