499185ace48fdd96b4abdfadf05d5a326f2e5f5e
Elte Hupkes <eltehupkes@srxp.com>
2015-07-19 12:48:15 +0200

    Adding simple world controller (unregistered) with insert capabilities to Revolve

---
+++ cpp/revolve/gazebo/plugin/WorldController.cpp
@@ -0,0 +1,39 @@
+//
+// Created by elte on 6-6-15.
+//
+
+#include "WorldController.h"
+#include <iostream>
+
+namespace gz = gazebo;
+
+namespace revolve {
+namespace gazebo {
+
+
+
+void WorldController::Load(gz::physics::WorldPtr world, sdf::ElementPtr /*_sdf*/) {
+	std::cout << "World plugin loaded." << std::endl;
+
+	// Store the world
+	world_ = world;
+
+	// Create transport node
+	node_.reset(new gz::transport::Node());
+	node_->Init();
+
+	// Subscribe to insert request messages
+	insertSub_ = node_->Subscribe("~/insert_robot_sdf/request",
+								  &WorldController::InsertRequest, this);
+}
+
+void WorldController::InsertRequest(ConstInsertSdfRobotRequestPtr & request) {
+	std::cout << "Insert request! Inserting SDF..." << std::endl;
+
+	sdf::SDF robotSDF;
+	robotSDF.SetFromString(request->sdf_contents());
+	world_->InsertModelSDF(robotSDF);
+}
+
+} // namespace gazebo
+} // namespace revolve
---
+++ cpp/revolve/gazebo/plugin/WorldController.h
@@ -0,0 +1,41 @@
+//
+// Created by elte on 6-6-15.
+//
+
+#ifndef REVOLVE_WORLDCONTROLLER_H
+#define REVOLVE_WORLDCONTROLLER_H
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/msgs/msgs.hh>
+
+#include "insert_sdf_robot.pb.h"
+
+namespace revolve {
+namespace gazebo {
+
+typedef const boost::shared_ptr<const ::revolve::msgs::InsertSdfRobotRequest> ConstInsertSdfRobotRequestPtr;
+
+class WorldController : public ::gazebo::WorldPlugin {
+public:
+	void Load(::gazebo::physics::WorldPtr _parent, sdf::ElementPtr _sdf);
+
+protected:
+	// Listener for analysis requests
+	void InsertRequest(ConstInsertSdfRobotRequestPtr &request);
+
+	// Stores the world
+	::gazebo::physics::WorldPtr world_;
+
+	// Transport node
+	::gazebo::transport::NodePtr node_;
+
+	// Subscriber
+	::gazebo::transport::SubscriberPtr insertSub_;
+};
+
+} // namespace gazebo
+} // namespace revolve
+
+#endif //REVOLVE_WORLDCONTROLLER_H
