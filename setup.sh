79961c70da0dd9dcbde57754e9ca7cc691ebe557
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2016-04-22 14:47:06 +0200

    setup.sh better structured

--- setup.sh
+++ setup.sh
@@ -7,19 +7,26 @@ function error_exit() {
   exit 1
 }
 
-function install() {
-  local usecase=$1
+function clone() {
+
+  if [ $# -gt 0 ]; then
+  local usecase="$1"
+    shift
+  else
+    error_exit "${LINENO}: No argument provided."
+  fi
+
   case "${usecase}" in
-    localisation)
+    "localisation"|"loc")
       git clone https://github.com/ci-group/revolve-localization.git localisation ;;
-    mating)
+    "mating"|"mat")
       git clone https://github.com/ci-group/revolve-mating-server.git mating ;;    
-    robot)
+    "robot"|"rob")
       git clone https://github.com/ci-group/revolve-brain.git brain
-      git clone https://github.com/ci-group/revolve-communication.git com
+      git clone https://github.com/ci-group/revolve-communication.git communication
       git clone https://github.com/ci-group/revolve-hal.git hal
       ;;
-    sim)
+    "simulation"|"sim")
       echo "Simulation not implemented currently." ;;
     *) error_exit "${LINENO}: Unexpected option: ${usecase}" ;;
   esac
@@ -27,20 +34,47 @@ function install() {
 }
 
 function help() {
-  echo "Help is underway!!!"
+  echo "Usage: ${PROGNAME} {clone|update|build|install|test} [usecase...]"
+  echo "Actions:"
+  echo "  clone        - clone designated usecase packages to project directory"
+  echo "  update       - update usecase packages to latest version"
+  echo "  build        - build libraries from usecase packages"
+  echo "  install      - install executables from usecase packages"
+  echo "  test         - run tests for usecase packages "
+  echo "Usecases:"
+  echo "  localisation (or loc)  - usecase for localisation module"
+  echo "  mating (or mat)        - usecase for mating module"
+  echo "  robot (or rob)         - usecase for robot module"
+  echo "  simulation (or sim)    - usecase for simulation module"
+  echo "Example: ${PROGNAME} clone mating"
 }
 
 function main() {
+
+  if [ $# -gt 0 ]; then
+    local acton="$1"
+    shift
+  else
+    error_exit "${LINENO}: No argument provided."
+  fi
+
   while [ $# -gt 0 ]; do
-    option="$1"
+    local option="$1"
     shift
     case "${option}" in
-      --install) install $1 ; break ;;
-      --help|-h) help ;;
-      --) break ;;
-      *) error_exit "${LINENO}: Unexpected option: ${option}" ;;
+      *) break ;;
     esac
-  done 
+  done
+
+  case "${acton}" in
+    "clone") clone ${option} ;;
+    "update") ;;
+    "build") ;;
+    "install") ;;
+    "test") ;;
+    *) help ;;
+  esac
+
   exit 0
 }
 
