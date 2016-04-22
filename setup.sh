7cc96385bbbcadef574091073923c5f3430962f5
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2016-04-22 17:58:42 +0200

    Implemented sketch of build method

--- setup.sh
+++ setup.sh
@@ -13,14 +13,18 @@ function clone() {
   local usecase="$1"
     shift
   else
-    error_exit "${LINENO}: No argument provided."
+    error_exit "${LINENO}: In ${FUNCNAME}() no usecase argument provided."
   fi
 
   case "${usecase}" in
     "localisation"|"loc")
       git clone https://github.com/ci-group/revolve-localization.git localisation ;;
     "mating"|"mat")
-      git clone https://github.com/ci-group/revolve-mating-server.git mating ;;    
+      git clone https://github.com/ci-group/revolve-mating-server.git mating ;;
+    "server"|"srv")
+      git clone https://github.com/ci-group/revolve-localization.git localisation
+      git clone https://github.com/ci-group/revolve-mating-server.git mating
+      ;;    
     "robot"|"rob")
       git clone https://github.com/ci-group/revolve-brain.git brain
       git clone https://github.com/ci-group/revolve-communication.git communication
@@ -28,7 +32,7 @@ function clone() {
       ;;
     "simulation"|"sim")
       echo "Simulation not implemented currently." ;;
-    *) error_exit "${LINENO}: Unexpected option: ${usecase}" ;;
+    *) error_exit "${LINENO}: In ${FUNCNAME}() unexpected usecase option: ${usecase}" ;;
   esac
   return 0
 }
@@ -37,6 +41,32 @@ function update() {
 
   find ./ -not -path '*/\.*' -type d -depth 1 -exec sh -c "git --git-dir={}/.git \
    --work-tree=$PWD/{} pull origin" \;
+  return 0
+}
+
+function build() {
+
+  local cwd=$(pwd)
+  if [ $# -gt 0 ]; then
+  local usecase="$1"
+    shift
+  else
+    error_exit "${LINENO}: In ${FUNCNAME}() no usecase argument provided."
+  fi
+
+  case "${usecase}" in
+    "localisation"|"loc")
+      echo "Build of localisation not implemented currently" ;;
+    "mating"|"mat")
+      echo "Build of mating not implemented currently" ;;
+    "server"|"srv")
+      echo "Build of localisation and mating not implemented currently" ;; 
+    "robot"|"rob")
+      echo "Build of robot not implemented currently" ;;
+    "simulation"|"sim")
+      echo "Build of simulation not implemented currently" ;;
+    *) error_exit "${LINENO}: In ${FUNCNAME}() unexpected usecase option: ${usecase}" ;;
+  esac
   return 0
 }
 
@@ -51,6 +81,7 @@ function help() {
   echo "Usecases:"
   echo "  localisation (or loc)  - usecase for localisation module"
   echo "  mating (or mat)        - usecase for mating module"
+  echo "  server (or srv)        - usecase for localisation and mating module"
   echo "  robot (or rob)         - usecase for robot module"
   echo "  simulation (or sim)    - usecase for simulation module"
   echo "Example: ${PROGNAME} clone mating"
@@ -62,7 +93,7 @@ function main() {
     local acton="$1"
     shift
   else
-    error_exit "${LINENO}: No argument provided."
+    error_exit "${LINENO}: In ${FUNCNAME}() no argument provided."
   fi
 
   while [ $# -gt 0 ]; do
@@ -76,7 +107,7 @@ function main() {
   case "${acton}" in
     "clone") clone ${option} ;;
     "update") update ;;
-    "build") ;;
+    "build") build ${option};;
     "install") ;;
     "test") ;;
     *) help ;;
