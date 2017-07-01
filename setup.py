ea913b84ac5992669558fc3b89da39bc0010178a
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2017-07-01 14:58:53 +0200

    Smalll fix to `setup.py`.

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='revolve',
-      version=0.2,
+      version='0.2',
       description='Revolve: robot evolution framework',
       author='Elte Hupkes',
       author_email='elte@hupkes.org',
-      url='https://github.com/ElteHupkes/revolve',
-      packages=['revolve', 'revolve.gazebo', 'revolve.build', 'revolve.build.sdf',
-                'revolve.convert', 'revolve.generate', 'revolve.spec', 'revolve.angle',
-                'revolve.util'],
-      install_requires=['PyYAML', 'pygazebo', 'protobuf', 'sdfbuilder', 'numpy', 'psutil']
+      url='https://github.com/ci-group/revolve',
+      packages=['revolve',
+                'revolve.angle',
+                'revolve.build',
+                'revolve.build.sdf',
+                'revolve.convert',
+                'revolve.gazebo',
+                'revolve.generate',
+                'revolve.spec',
+                'revolve.util'
+               ],
+      install_requires=['numpy',
+                        'protobuf',
+                        'pygazebo',
+                        'PyYAML',
+                        'sdfbuilder',
+                        'psutil'
+                       ]
       )
