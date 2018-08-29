b0365bdebc19c01b4fef6a8659635aef6fe158d2
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2018-08-29 13:40:41 +0200

    Format `NeuralNetwork.h` according to C++ lint rules

--- cpp/revolve/gazebo/brain/NeuralNetwork.h
+++ cpp/revolve/gazebo/brain/NeuralNetwork.h
@@ -22,10 +22,15 @@
 #ifndef REVOLVE_GAZEBO_BRAIN_NEURALNETWORK_H_
 #define REVOLVE_GAZEBO_BRAIN_NEURALNETWORK_H_
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include <gazebo/gazebo.hh>
+
+#include <revolve/msgs/neural_net.pb.h>
 
 #include "Brain.h"
-#include <gazebo/gazebo.hh>
-#include <revolve/msgs/neural_net.pb.h>
 
 // These numbers are quite arbitrary. It used to be in:13 out:8
 // for the Arduino, but I upped them both to 20 to accomodate other
@@ -43,144 +48,162 @@
 // (bias, tau, gain) or (phase offset, period, gain)
 #define MAX_NEURON_PARAMS 3
 
-namespace revolve {
-namespace gazebo {
-
-typedef const boost::shared_ptr<revolve::msgs::ModifyNeuralNetwork const> ConstModifyNeuralNetworkPtr;
+namespace revolve
+{
+  namespace gazebo
+  {
+    typedef const boost::shared_ptr< revolve::msgs::ModifyNeuralNetwork const >
+            ConstModifyNeuralNetworkPtr;
 
 /*
  * Copied from NeuronRepresentation.h
  */
-enum neuronType{
-	INPUT,
-	SIMPLE,
-	SIGMOID,
-	CTRNN_SIGMOID,
-	OSCILLATOR,
-	SUPG
-};
+    enum neuronType
+    {
+      INPUT,
+      SIMPLE,
+      SIGMOID,
+      CTRNN_SIGMOID,
+      OSCILLATOR,
+      SUPG
+    };
 
-class NeuralNetwork: public Brain {
-public:
-	/**
-	 * @param Name of the robot
-	 * @param The brain node
-	 * @param Reference to motor list, which might be reordered
-	 * @param Reference to the sensor list, which might be reordered
-	 */
-	NeuralNetwork(std::string modelName, sdf::ElementPtr node,
-				  std::vector< MotorPtr > & motors, std::vector< SensorPtr > & sensors);
-	virtual ~NeuralNetwork();
+    class NeuralNetwork
+            : public Brain
+    {
+      public:
+      /**
+       * @param Name of the robot
+       * @param The brain node
+       * @param Reference to motor list, which might be reordered
+       * @param Reference to the sensor list, which might be reordered
+       */
+      NeuralNetwork(
+              std::string modelName,
+              sdf::ElementPtr node,
+              std::vector< MotorPtr > &motors,
+              std::vector< SensorPtr > &sensors);
 
-   /**
-	* @param Motor list
-	* @param Sensor list
-	*/
-	virtual void update(const std::vector< MotorPtr > & motors, const std::vector< SensorPtr > & sensors,
-			double t, double step);
+      virtual ~NeuralNetwork();
 
-protected:
-	/**
-	 * Steps the neural network
-	 */
-	void step(double time);
+      /**
+     * @param Motor list
+     * @param Sensor list
+     */
+      virtual void update(
+              const std::vector< MotorPtr > &motors,
+              const std::vector< SensorPtr > &sensors,
+              double t,
+              double step);
 
-	/**
-	 * Request handler to modify the neural network
-	 */
-	void modify(ConstModifyNeuralNetworkPtr &req);
+      protected:
+      /**
+       * Steps the neural network
+       */
+      void step(double time);
 
-	// Mutex for stepping / updating the network
-	boost::mutex networkMutex_;
+      /**
+       * Request handler to modify the neural network
+       */
+      void modify(ConstModifyNeuralNetworkPtr &req);
 
-	/**
-	 * Transport node
-	 */
-	::gazebo::transport::NodePtr node_;
+      // Mutex for stepping / updating the network
+      boost::mutex networkMutex_;
 
-	/**
-	 * Network modification subscriber
-	 */
-	::gazebo::transport::SubscriberPtr alterSub_;
+      /**
+       * Transport node
+       */
+      ::gazebo::transport::NodePtr node_;
 
-	/*
-	 * Connection weights, separated into three arrays for convenience. Note
-	 * that only output and hidden neurons are weight targets.
-	 *
-	 * Weights are stored with gaps, meaning that every neuron holds entries for
-	 * the maximum possible number of connections. This makes restructuring the
-	 * weights arrays when a hidden neuron is removed slightly less cumbersome.
-	 */
-	double inputWeights_[MAX_INPUT_NEURONS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
-	double outputWeights_[MAX_OUTPUT_NEURONS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
-	double hiddenWeights_[MAX_HIDDEN_NEURONS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
+      /**
+       * Network modification subscriber
+       */
+      ::gazebo::transport::SubscriberPtr alterSub_;
 
-	// Unlike weights, types, params and current states are stored without
-	// gaps, meaning the first `m` entries are for output neurons, followed
-	// by `n` entries for hidden neurons. If a hidden neuron is removed,
-	// the items beyond it are moved back.
-	/**
-	 * Type of each non-input neuron
-	 */
-	unsigned int types_[(MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
+      /*
+       * Connection weights, separated into three arrays for convenience. Note
+       * that only output and hidden neurons are weight targets.
+       *
+       * Weights are stored with gaps, meaning that every neuron holds entries for
+       * the maximum possible number of connections. This makes restructuring the
+       * weights arrays when a hidden neuron is removed slightly less cumbersome.
+       */
+      double inputWeights_[
+              MAX_INPUT_NEURONS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
+      double outputWeights_[
+              MAX_OUTPUT_NEURONS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
+      double hiddenWeights_[
+              MAX_HIDDEN_NEURONS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
 
-	/*
-	 * Params for hidden and output neurons, quantity depends on the type of
-	 * neuron
-	 */
-	double params_[MAX_NEURON_PARAMS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
+      // Unlike weights, types, params and current states are stored without
+      // gaps, meaning the first `m` entries are for output neurons, followed
+      // by `n` entries for hidden neurons. If a hidden neuron is removed,
+      // the items beyond it are moved back.
+      /**
+       * Type of each non-input neuron
+       */
+      unsigned int types_[(MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
 
-	/*
-	 * Output states arrays for the current state and the next state.
-	 */
-	double state1_[MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS];
-	double state2_[MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS];
+      /*
+       * Params for hidden and output neurons, quantity depends on the type of
+       * neuron
+       */
+      double params_[
+              MAX_NEURON_PARAMS * (MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS)];
 
-	/**
-	 * One input state for each input neuron
-	 */
-	double input_[MAX_INPUT_NEURONS];
+      /*
+       * Output states arrays for the current state and the next state.
+       */
+      double state1_[MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS];
+      double state2_[MAX_OUTPUT_NEURONS + MAX_HIDDEN_NEURONS];
 
-	/**
-	 * Used to determine the current state array. False = state1,
-	 * true = state2.
-	 */
-	bool flipState_;
+      /**
+       * One input state for each input neuron
+       */
+      double input_[MAX_INPUT_NEURONS];
 
-	/**
-	 * Stores the type of each neuron ID
-	 */
-	std::map<std::string, std::string> layerMap_;
+      /**
+       * Used to determine the current state array. False = state1,
+       * true = state2.
+       */
+      bool flipState_;
 
-	/**
-	 * Stores the position of each neuron ID, relative to its type
-	 */
-	std::map<std::string, unsigned int> positionMap_;
+      /**
+       * Stores the type of each neuron ID
+       */
+      std::map< std::string, std::string > layerMap_;
 
-	/**
-	 * The number of inputs
-	 */
-	unsigned int nInputs_;
+      /**
+       * Stores the position of each neuron ID, relative to its type
+       */
+      std::map< std::string, unsigned int > positionMap_;
 
-	/**
-	 * The number of outputs
-	 */
-	unsigned int nOutputs_;
+      /**
+       * The number of inputs
+       */
+      unsigned int nInputs_;
 
-	/**
-	 * The number of hidden units
-	 */
-	unsigned int nHidden_;
+      /**
+       * The number of outputs
+       */
+      unsigned int nOutputs_;
 
-	/**
-	 * The number of non-inputs (i.e. nOutputs + nHidden)
-	 */
-	unsigned int nNonInputs_;
-private:
-	void connectionHelper(const std::string & src, const std::string & dst, double weight);
-};
+      /**
+       * The number of hidden units
+       */
+      unsigned int nHidden_;
 
-} /* namespace gazebo */
+      /**
+       * The number of non-inputs (i.e. nOutputs + nHidden)
+       */
+      unsigned int nNonInputs_;
+      private:
+      void connectionHelper(
+              const std::string &src,
+              const std::string &dst,
+              double weight);
+    };
+  } /* namespace gazebo */
 } /* namespace revolve */
 
 #endif /* REVOLVE_GAZEBO_BRAIN_NEURALNETWORK_H_ */
