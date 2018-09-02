73d9723d942640078f307c1f9c7569bab48a7641
Milan Jelisavcic <milan.jelisavcic@gmail.com>
2018-09-02 20:54:50 +0200

    Format `LightSensor.h` according to C++ lint rules

--- cpp/revolve/gazebo/sensors/LightSensor.h
+++ cpp/revolve/gazebo/sensors/LightSensor.h
@@ -21,49 +21,57 @@
 #ifndef REVOLVE_GAZEBO_SENSORS_LIGHTSENSOR_H_
 #define REVOLVE_GAZEBO_SENSORS_LIGHTSENSOR_H_
 
+#include <string>
+
 #include <revolve/gazebo/sensors/Sensor.h>
 
-namespace revolve {
-namespace gazebo {
+namespace revolve
+{
+  namespace gazebo
+  {
+    class LightSensor
+            : public Sensor
+    {
+      public:
+      LightSensor(
+              ::gazebo::physics::ModelPtr model,
+              sdf::ElementPtr sensor,
+              std::string partId,
+              std::string sensorId);
 
-class LightSensor: public Sensor {
-public:
-	LightSensor(::gazebo::physics::ModelPtr model, sdf::ElementPtr sensor,
-			std::string partId, std::string sensorId);
-	virtual ~LightSensor();
+      virtual ~LightSensor();
 
-	/**
-	 * Returns a float intensity between 0 and 1
-	 */
-	virtual void read(double * input);
+      /**
+       * Returns a float intensity between 0 and 1
+       */
+      virtual void read(double *input);
 
-	/**
-	 * Called when the camera sensor is updated
-	 */
-	void OnUpdate();
+      /**
+       * Called when the camera sensor is updated
+       */
+      void OnUpdate();
 
-private:
-	/**
-	 * Sensor dynamically casted to correct type,
-	 * so it needs to happen only once.
-	 */
-	::gazebo::sensors::CameraSensorPtr castSensor_;
+      private:
+      /**
+       * Sensor dynamically casted to correct type,
+       * so it needs to happen only once.
+       */
+      ::gazebo::sensors::CameraSensorPtr castSensor_;
 
-	/**
-	 * Size of the data which we determine once
-	 */
-	unsigned int dataSize_;
+      /**
+       * Size of the data which we determine once
+       */
+      unsigned int dataSize_;
 
-	/**
-	 * Last calculated average
-	 */
-	float lastValue_;
+      /**
+       * Last calculated average
+       */
+      float lastValue_;
 
-    // Pointer to the update connection
-    ::gazebo::event::ConnectionPtr updateConnection_;
-};
-
-} /* namespace gazebo */
+      // Pointer to the update connection
+      ::gazebo::event::ConnectionPtr updateConnection_;
+    };
+  } /* namespace gazebo */
 } /* namespace revolve */
 
 #endif /* REVOLVE_GAZEBO_SENSORS_LIGHTSENSOR_H_ */
