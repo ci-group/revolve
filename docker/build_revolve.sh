b079872edc1280a880eb15c2bf446b8d5fe5feb6
Karine <karine.smiras@gmail.com>
2019-02-14 14:12:44 +0100

    fix find protobuf files in cmake

--- docker/build_revolve.sh
+++ docker/build_revolve.sh
@@ -4,8 +4,7 @@ set -e
 # Build Revolve
 cd /revolve
 mkdir -p build && cd build
-cmake ../cpprevolve \
-      -DCMAKE_BUILD_TYPE="Release"
+cmake .. -DCMAKE_BUILD_TYPE="Release"
 make -j4
 
 # Install the Python dependencies
