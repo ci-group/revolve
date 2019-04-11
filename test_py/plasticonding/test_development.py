784ea2ca92306d2b63dfd87640707e71d2df0368
Matteo De Carlo <matteo.dek@covolunablu.org>
2019-04-11 18:32:31 +0200

    fix measurements unit tests

--- test_py/plasticonding/test_development.py
+++ test_py/plasticonding/test_development.py
@@ -33,7 +33,7 @@ class TestPlastiCoding(unittest.TestCase):
 
         self.genotype.export_genotype(file1)
 
-        genotype2 = pyrevolve.genotype.plasticoding.plasticoding.initialization.random_initialization(self.conf)
+        genotype2 = pyrevolve.genotype.plasticoding.plasticoding.Plasticoding(self.conf)
         genotype2.id = self.genotype.id
         genotype2.load_genotype(file1)
         genotype2.export_genotype(file2)
@@ -54,7 +54,7 @@ class TestPlastiCoding(unittest.TestCase):
         robot.update_substrate(raise_for_intersections=True)
 
 
-class Test180(unittest.TestCase):
+class Test176(unittest.TestCase):
     def setUp(self):
         self.conf = pyrevolve.genotype.plasticoding.plasticoding.PlasticodingConfig()
 
@@ -81,45 +81,48 @@ class Test180(unittest.TestCase):
         total_components = 0.11
         total_components_abs = 11
 
+        self.robot.render2d('/tmp/robot.png')
+        self.genotype.export_genotype('/tmp/cacca.txt')
+
         m = self.robot.measure_body()
-        self.assertAlmostEqual(branching, m.branching)
-        self.assertAlmostEqual(connectivity1, m.limbs)
-        self.assertAlmostEqual(connectivity1_abs, m.extremities)
-        self.assertAlmostEqual(connectivity2, m.length_of_limbs)
-        self.assertAlmostEqual(connectivity2_abs, m.extensiveness)
-        # self.assertAlmostEqual(connectivity3, m.)
-        self.assertAlmostEqual(connectivity4, m.branching_modules_count)
-        self.assertAlmostEqual(coverage, m.coverage)
-        self.assertAlmostEqual(effective_joints, m.joints)
-        self.assertAlmostEqual(joints_abs, m.hinge_count)
-        self.assertAlmostEqual(length_ratio, m.proportion)
-        self.assertAlmostEqual(sensors, m.sensors)
-        self.assertAlmostEqual(symmetry, m.symmetry)
-        self.assertAlmostEqual(total_components, m.size)
-        self.assertAlmostEqual(total_components_abs, m.absolute_size)
+        self.assertAlmostEqual(branching, m.branching, 3)
+        self.assertAlmostEqual(connectivity1, m.limbs, 3)
+        self.assertAlmostEqual(connectivity1_abs, m.extremities, 3)
+        self.assertAlmostEqual(connectivity2, m.length_of_limbs, 3)
+        self.assertAlmostEqual(connectivity2_abs, m.extensiveness, 3)
+        # self.assertAlmostEqual(connectivity3, m., 3)
+        self.assertAlmostEqual(connectivity4, m.branching_modules_count, 3)
+        self.assertAlmostEqual(coverage, m.coverage, 3)
+        self.assertAlmostEqual(effective_joints, m.joints, 3)
+        self.assertAlmostEqual(joints_abs, m.hinge_count, 3)
+        self.assertAlmostEqual(length_ratio, m.proportion, 3)
+        self.assertAlmostEqual(sensors, m.sensors, 3)
+        self.assertAlmostEqual(symmetry, m.symmetry, 3)
+        self.assertAlmostEqual(total_components, m.size, 3)
+        self.assertAlmostEqual(total_components_abs, m.absolute_size, 3)
 
     def test_measurements_brain(self):
         amplitude_average = 0.551664
-        amplitude_deviation = 0.914862
+        amplitude_deviation = 0.915769767
         inputs_reach = 0.583333
-        inter_params_dev_average = 0.875401
-        intra_params_dev_average = 0.791891
-        offset_average = 0.355431
-        offset_deviation = 0.852846
-        period_average = 0.258451
-        period_deviation = 0.847762
+        inter_params_dev_average = 0.8811009266695952
+        intra_params_dev_average = 0.8187877255612809
+        offset_average = 0.37370865000000003
+        offset_deviation = 0.857319047568582
+        period_average = 0.25102035
+        period_deviation = 0.8616760913682427
         recurrence = 0.5
         synaptic_reception = 0
 
         m = self.robot.measure_brain()
-        self.assertAlmostEqual(amplitude_average, m.avg_amplitude)
-        self.assertAlmostEqual(amplitude_deviation, m.dev_amplitude)
-        self.assertAlmostEqual(inputs_reach, m.sensors_reach)
-        self.assertAlmostEqual(inter_params_dev_average, m.avg_inter_dev_params)
-        self.assertAlmostEqual(intra_params_dev_average, m.avg_intra_dev_params)
-        self.assertAlmostEqual(offset_average, m.avg_phase_offset)
-        self.assertAlmostEqual(offset_deviation, m.dev_phase_offset)
-        self.assertAlmostEqual(period_average, m.avg_period)
-        self.assertAlmostEqual(period_deviation, m.dev_period)
-        self.assertAlmostEqual(recurrence, m.recurrence)
-        self.assertAlmostEqual(synaptic_reception, m.synaptic_reception)
+        self.assertAlmostEqual(amplitude_average, m.avg_amplitude, 3)
+        self.assertAlmostEqual(amplitude_deviation, m.dev_amplitude, 3)
+        self.assertAlmostEqual(inputs_reach, m.sensors_reach, 3)
+        self.assertAlmostEqual(inter_params_dev_average, m.avg_inter_dev_params, 3)
+        self.assertAlmostEqual(intra_params_dev_average, m.avg_intra_dev_params, 3)
+        self.assertAlmostEqual(offset_average, m.avg_phase_offset, 3)
+        self.assertAlmostEqual(offset_deviation, m.dev_phase_offset, 3)
+        self.assertAlmostEqual(period_average, m.avg_period, 3)
+        self.assertAlmostEqual(period_deviation, m.dev_period, 3)
+        self.assertAlmostEqual(recurrence, m.recurrence, 3)
+        self.assertAlmostEqual(synaptic_reception, m.synaptic_reception, 3)
