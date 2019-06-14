2e255e04486c5d09e4e0083e6b37f11f3dea5b12
Matteo De Carlo <matteo.dek@gmail.com>
2019-06-14 11:06:21 +0200

    Restructured CMAKE files

---
+++ cpprevolve/revolve/brains/controller/DifferentialCPG.h
@@ -0,0 +1,14 @@
+//
+// Created by matteo on 14/06/19.
+//
+
+#ifndef REVOLVE_DIFFERENTIALCPG_H
+#define REVOLVE_DIFFERENTIALCPG_H
+
+
+class DifferentialCPG {
+
+};
+
+
+#endif //REVOLVE_DIFFERENTIALCPG_H
