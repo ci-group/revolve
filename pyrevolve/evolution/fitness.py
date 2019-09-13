c766609ed33e08d079d8f3734d281c656968a774
Karine <karine.smiras@gmail.com>
2019-09-13 17:08:00 +0200

    small fitness change

--- pyrevolve/evolution/fitness.py
+++ pyrevolve/evolution/fitness.py
@@ -57,6 +57,23 @@ def size_penalty(robot_manager, robot):
 
     return _size_penalty
 
+def displacement_velocity_hill2(robot_manager, robot, cost=False):
+    fitness = measures.displacement_velocity_hill(robot_manager)
+
+     if fitness == 0:
+        fitness = -0.1
+
+    elif fitness < 0:
+        fitness /= 10
+
+    if cost and fitness != None:
+        _size_penalty = size_penalty(robot_manager, robot) ** 2
+        if fitness >= 0:
+            fitness = fitness * _size_penalty
+        else:
+            fitness = fitness / _size_penalty
+
+    return fitness
 
 def displacement_velocity_hill(robot_manager, robot, cost=False):
     fitness = measures.displacement_velocity_hill(robot_manager)
