0de25b7a23b488a58a35c7d71e89e5fdbccad53f
Andi <andreas@sauter.at>
2019-09-24 18:39:36 +0200

    Fixed type/pointer issues and minimal refactoring.

--- cpprevolve/revolve/gazebo/brains/DifferentialCPGClean.h
+++ cpprevolve/revolve/gazebo/brains/DifferentialCPGClean.h
@@ -13,44 +13,34 @@ namespace revolve
 {
     namespace gazebo
     {
+        std::vector<std::unique_ptr<revolve::Actuator>> convertMotorsToActuators(const std::vector<MotorPtr> &_motors)
+        {
+            std::vector<std::unique_ptr<revolve::Actuator>> actuators;
+            actuators.reserve(_motors.size());
+            for (auto &motor: _motors) {
+                actuators.push_back(std::make_unique<ActuatorWrapper>(motor.get(), 0, 0, 0));
+            }
+
+            return actuators;
+        }
 
         class DifferentialCPGClean: public Brain, private revolve::DifferentialCPG
         {
-        private:
-            std::vector<std::unique_ptr<Actuator>> convertMotorToActuator(&_motors)
-            {
-                std::vector<std::unique_ptr<revolve::Actuator>> actuators;
-                actuators.reserve(_motors.size());
-                for (auto &motor: _motors) {
-                    actuators.push_back(std::make_unique<ActuatorWrapper>(motor.get(), 0, 0, 0));
-                }
-                return actuators;
-            }
         public:
             explicit DifferentialCPGClean(const revolve::DifferentialCPG::ControllerParams &params,
                                           const std::vector< MotorPtr > &_motors)
                     : Brain()
-                    , revolve::DifferentialCPG(&params, convertMotorToActuator(&_motors));
-            {
-                /*std::vector<std::unique_ptr<revolve::Actuator>> actuators;
-                actuators.reserve(_motors.size());
-                for (auto &motor: _motors) {
-                    actuators.push_back(std::make_unique<ActuatorWrapper>(motor.get(), 0, 0, 0));
-                }
-                revolve::DifferentialCPG(params, actuators);*/
-            }
+                    , revolve::DifferentialCPG(params, revolve::gazebo::convertMotorsToActuators(_motors))
+            {}
 
             void Update(const std::vector<MotorPtr> &_motors,
                         const std::vector<SensorPtr> &_sensors,
                         const double _time,
                         const double _step) override
             {
-                std::vector<std::unique_ptr<revolve::Actuator>> actuators;
-                std::vector<std::unique_ptr<revolve::Sensor>> sensors;
-                actuators.reserve(_motors.size());
-                for (auto &motor: _motors) {
-                    actuators.push_back(std::make_unique<ActuatorWrapper>(motor.get(), 0, 0, 0));
-                }
+                std::vector<std::unique_ptr<revolve::Actuator>> actuators = revolve::gazebo::convertMotorsToActuators(_motors);
+                std::vector<std::unique_ptr<revolve::Sensor>> sensors;  // CPG does not actually have any sensors.
+
                 revolve::DifferentialCPG::update(actuators, sensors, _time, _step);
             }
         };
