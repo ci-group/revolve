ed685ea689cd8fad4ac08d8424db4fff2edc3089
Karine <karine.smiras@gmail.com>
2020-01-10 18:44:44 +0100

    adapts scripts od analysis

--- experiments/karines_experiments/consolidate_experiments.py
+++ experiments/karines_experiments/consolidate_experiments.py
@@ -1,20 +1,23 @@
 import os
-
+import math
 
 # set these variables according to your experiments #
-dirpath = 'data'
+dirpath = 'data/'
 experiments_type = [
-                    'plane',
-                    'lava'
+                    'baseline',
+                    'plastic'
                     ]
-runs = 10
+environments = [
+                'plane',
+                'tilted5'
+               ]
+runs = 30
+
 # set these variables according to your experiments #
 
+def build_headers(path1, path2):
 
-def build_headers(path):
-
-    print(path + "/all_measures.txt")
-    file_summary = open(path + "/all_measures.tsv", "w+")
+    file_summary = open(path1 + "/all_measures.tsv", "w+")
     file_summary.write('robot_id\t')
 
     behavior_headers = []
@@ -36,7 +39,7 @@ def build_headers(path):
     #         file_summary.write(measure+'\t')
 
     phenotype_headers = []
-    with open(path + '/data_fullevolution/descriptors/phenotype_desc_robot_1.txt') as file:
+    with open(path1 + '/descriptors/phenotype_desc_robot_1.txt') as file:
         for line in file:
             measure, value = line.strip().split(' ')
             phenotype_headers.append(measure)
@@ -44,64 +47,69 @@ def build_headers(path):
     file_summary.write('fitness\n')
     file_summary.close()
 
-    file_summary = open(path + "/snapshots_ids.tsv", "w+")
+    file_summary = open(path2 + "/snapshots_ids.tsv", "w+")
     file_summary.write('generation\trobot_id\n')
     file_summary.close()
 
     return behavior_headers, phenotype_headers
 
-
 for exp in experiments_type:
-    for run in range(1, runs+1):
 
-        print(exp, run)
-        path = os.path.join(dirpath, str(exp), str(run))
-        behavior_headers, phenotype_headers = build_headers(path)
+    for env in environments:
 
-        file_summary = open(path + "/all_measures.tsv", "a")
-        for r, d, f in os.walk(path+'/data_fullevolution/fitness'):
-            for file in f:
+        for run in range(1, runs+1):
 
-                robot_id = file.split('.')[0].split('_')[-1]
-                file_summary.write(robot_id+'\t')
 
-                bh_file = path+'/data_fullevolution/descriptors/behavior_desc_robot_'+robot_id+'.txt'
-                if os.path.isfile(bh_file):
-                    with open(bh_file) as file:
-                        for line in file:
-                            if line != 'None':
+            path1 = dirpath + str(exp) + '_' + str(run) + '/data_fullevolution/' + env
+            path2 = dirpath + str(exp) + '_' + str(run) + '/selectedpop_' + env
+
+            behavior_headers, phenotype_headers = build_headers(path1, path2)
+
+            file_summary = open(path1 + "/all_measures.tsv", "a")
+            for r, d, f in os.walk(path1+'/fitness'):
+                for file in f:
+
+                    robot_id = file.split('.')[0].split('_')[-1]
+                    file_summary.write(robot_id+'\t')
+
+                    bh_file = path1+'/descriptors/behavior_desc_robot_'+robot_id+'.txt'
+                    if os.path.isfile(bh_file):
+                        with open(bh_file) as file:
+                            for line in file:
+                                if line != 'None':
+                                    measure, value = line.strip().split(' ')
+                                    file_summary.write(value+'\t')
+                                else:
+                                    for h in behavior_headers:
+                                        file_summary.write('None'+'\t')
+                    else:
+                        for h in behavior_headers:
+                            file_summary.write('None'+'\t')
+
+                    pt_file = path1+'/descriptors/phenotype_desc_robot_'+robot_id+'.txt'
+                    if os.path.isfile(pt_file):
+                        with open(pt_file) as file:
+                            for line in file:
                                 measure, value = line.strip().split(' ')
                                 file_summary.write(value+'\t')
-                            else:
-                                for h in behavior_headers:
-                                    file_summary.write('None'+'\t')
-                else:
-                    for h in behavior_headers:
-                        file_summary.write('None'+'\t')
+                    else:
+                        for h in phenotype_headers:
+                            file_summary.write('None'+'\t')
 
-                pt_file = path+'/data_fullevolution/descriptors/phenotype_desc_robot_'+robot_id+'.txt'
-                if os.path.isfile(pt_file):
-                    with open(pt_file) as file:
-                        for line in file:
-                            measure, value = line.strip().split(' ')
-                            file_summary.write(value+'\t')
-                else:
-                    for h in phenotype_headers:
-                        file_summary.write('None'+'\t')
+                    file = open(path1+'/fitness/fitness_robot_'+robot_id+'.txt', 'r')
+                    fitness = file.read()
+                    file_summary.write(fitness + '\n')
+            file_summary.close()
+            print(exp, run, len(f), round(((len(f)-100)/50),0)+1)
 
-                file = open(path+'/data_fullevolution/fitness/fitness_robot_'+robot_id+'.txt', 'r')
-                fitness = file.read()
-                file_summary.write(fitness + '\n')
-        file_summary.close()
-
-        file_summary = open(path + "/snapshots_ids.tsv", "a")
-        for r, d, f in os.walk(path):
-            for dir in d:
-                if 'selectedpop' in dir:
-                    gen = dir.split('_')[1]
-                    for r2, d2, f2 in os.walk(path + '/selectedpop_' + str(gen)):
-                        for file in f2:
-                            if 'body' in file:
-                                id = file.split('.')[0].split('_')[-1]
-                                file_summary.write(gen+'\t'+id+'\n')
-        file_summary.close()
+            file_summary = open(path2 + "/snapshots_ids.tsv", "a")
+            for r, d, f in os.walk(path2):
+                for dir in d:
+                    if 'selectedpop' in dir:
+                        gen = dir.split('_')[1]
+                        for r2, d2, f2 in os.walk(path2 + '/selectedpop_' + str(gen)):
+                            for file in f2:
+                                if 'body' in file:
+                                    id = file.split('.')[0].split('_')[-1]
+                                    file_summary.write(gen+'\t'+id+'\n')
+            file_summary.close()
