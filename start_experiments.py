c829e4e2e9dc2a8604a4753e28ce8b41c9e6058d
SamFerwerda <sam.ferwerda@hotmail.com>
2020-05-28 14:43:25 +0200

    added start_experiment.py for quick parallel experiments

---
+++ start_experiments.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import subprocess
+
+runs=[1,2,6,7,8,9,10]#range(1,11)
+start_port=14000
+exp_name='SpeedUpAnalyzer'
+log_suffix=''
+manager='experiments/examples/manager_pop.py'
+
+if __name__ == "__main__":
+    for run in runs:
+        run_start_port = start_port + (run*10)
+        process = ['screen','-d','-m',
+            '-S',f'{exp_name}_{run}',
+            '-L','-Logfile',f"{exp_name}{log_suffix}_{run}.log",
+            'nice','-n19',
+            './revolve.sh','--manager',manager,
+            '--experiment-name',exp_name,
+            '--n-cores','4',
+            '--port-start',str(run_start_port),
+            '--run',str(run)
+            ]
+        print("starting" + " ".join(process))
+        subprocess.call(process)
