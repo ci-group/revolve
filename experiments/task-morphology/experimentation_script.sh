282b1815420e21233ebfe6502234e0077665c4d3
Matteo De Carlo <matteo.dek@covolunablu.org>
2020-06-19 18:54:52 +0200

    Update Experimentation script involving the fitness function

--- experiments/task-morphology/experimentation_script.sh
+++ experiments/task-morphology/experimentation_script.sh
@@ -16,14 +16,14 @@ set -x
 
 runs=10
 runs_start=0
-start_port=15000
-exp_name=Experiment_A
-fitness=displacement
+start_port=13000
+exp_name=Experiment_B
+fitness=rotation
 log_suffix=''
 manager=experiments/task-morphology/manager.py
 
 for i in $(seq $runs)
 do
 	run=$(($i+runs_start))
-	screen -d -m -S "${exp_name}_${run}" -L -Logfile "${exp_name}${log_suffix}_${run}.log" nice -n19 ./revolve.sh --manager $manager --experiment-name $exp_name --n-cores 4 --port-start $((${start_port} + ($run*10))) --run $run
+	screen -d -m -S "${exp_name}_${run}" -L -Logfile "${exp_name}${log_suffix}_${run}.log" nice -n19 ./revolve.sh --manager $manager --experiment-name $exp_name --fitness $fitness --n-cores 4 --port-start $((${start_port} + ($run*10))) --run $run
 done
