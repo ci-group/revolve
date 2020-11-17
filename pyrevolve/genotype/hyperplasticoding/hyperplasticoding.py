f387ebeba0a7c27e0d266002101395a8715d699a
Karine <karine.smiras@gmail.com>
2020-11-17 23:03:29 +0100

    decode body - part1

--- pyrevolve/genotype/hyperplasticoding/hyperplasticoding.py
+++ pyrevolve/genotype/hyperplasticoding/hyperplasticoding.py
@@ -13,6 +13,9 @@ import math
 import neat
 import os
 import random
+import pprint
+from matplotlib.pyplot import figure
+from matplotlib.ticker import MaxNLocator
 
 
 class Alphabet(Enum):
@@ -88,32 +91,127 @@ class HyperPlasticoding:
 
     def develop_body(self):
 
-        radius = int((self.conf.substrate_length-1)/2)
+        # size of substrate is (substrate_radius*2+1)^2
+        radius = self.conf.substrate_radius
         cppn = neat.nn.FeedForwardNetwork.create(self.cppn_body, self.body_config)
 
-        # for each column in the 2d space of the body
-        for x in range(-radius, radius+1):
-            # for each column row
-            for y in range(-radius, radius+1):
-                self.query_body(x, y, radius, cppn)
+        self.query_body(radius, cppn)
 
-        print(self.substrate)
-        print(self.phenotype)
+        self.attach_body()
+
+    def attach_body(self):
+
+        for point in self.substrate:
+            valid_attachment = True
+
+            # head has no parent
+            if point == (0, 0):
+                valid_attachment = False
+            else:
+
+                if self.substrate[point]['parent_direction'] == Orientation.NORTH:
+                    parent_point = tuple(map(sum, zip(point, (0, 1))))
+                    parent_slot = Orientation.SOUTH
+
+                elif self.substrate[point]['parent_direction'] == Orientation.SOUTH:
+                    parent_point = tuple(map(sum, zip(point, (0, -1))))
+                    parent_slot = Orientation.NORTH
+
+                elif self.substrate[point]['parent_direction'] == Orientation.WEST:
+                    parent_point = tuple(map(sum, zip(point, (-1, 0))))
+                    parent_slot = Orientation.EAST
+
+                elif self.substrate[point]['parent_direction'] == Orientation.EAST:
+                    parent_point = tuple(map(sum, zip(point, (1, 0))))
+                    parent_slot = Orientation.WEST
+
+                # coordinates candidate to parent are empty or out of substrate
+                if parent_point not in self.substrate.keys():
+                    valid_attachment = False
+                else:
+                    # sensors are terminals and thus can not be parents
+                    if self.substrate[parent_point]['module'].info['module_type'] == Alphabet.SENSOR:
+                        valid_attachment = False
+
+                    # joints do not connect to sensors
+                    # TODO: allow that in the future?
+                    elif self.substrate[parent_point]['module'].info['module_type'] \
+                            in (Alphabet.JOINT_VERTICAL, Alphabet.JOINT_HORIZONTAL) \
+                            and self.substrate[point]['module'].info['module_type'] == Alphabet.SENSOR:
+                        valid_attachment = False
+
+            # attaching module to due parent slot
+            if valid_attachment:
+
+                self.substrate[parent_point]['module'].children[parent_slot.value] = self.substrate[point]['module']
+
+                self.substrate[point]['module'].orientation =\
+                    self.get_angle(self.substrate[point]['module'].info['module_type'],
+                                   self.substrate[parent_point]['module'])
+
+                if self.substrate[point]['module'].info['module_type'] != Alphabet.SENSOR:
+                    # TODO: check if it is skipping some cases
+                    self.quantity_modules += 1
+                    self.substrate[point]['module'].id = str(self.quantity_modules)
+
+    def query_body(self, radius, cppn):
+
+        # for each column in the 2d space (left to right)
+        for x in range(-radius, radius+1):
+            # for each row of the column (bottom to top)
+            for y in range(-radius, radius+1):
+                print(x, y)
+                self.query_body_part(x, y, radius, cppn)
+
+        self.draw_queried_substrate()
+
+    def draw_queried_substrate(self):
+
+        x = []
+        y = []
+        markers = []
+        color = []
+        for point in self.substrate:
+            print(point,self.substrate[point]['parent_direction'])
+            x.append(point[0])
+            y.append(point[1])
+
+            if self.substrate[point]['parent_direction'] == Orientation.SOUTH:
+                markers.append('v')
+            elif self.substrate[point]['parent_direction'] == Orientation.NORTH:
+                markers.append('^')
+            elif self.substrate[point]['parent_direction'] == Orientation.WEST:
+                markers.append('<')
+            elif self.substrate[point]['parent_direction'] == Orientation.EAST:
+                markers.append('>')
+            else:
+                markers.append('o')
+
+            color.append(tuple(self.get_color(self.substrate[point]['module'].info['module_type'])))
+
+        ax = figure().gca()
+        for xp, yp, m, c in zip(x, y, markers, color):
+            ax.scatter([xp], [yp], marker=m, color=c, s=400)
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.get_figure().savefig('t.png')
 
     def place_head(self):
 
+        module_type = Alphabet.CORE_COMPONENT
         module = CoreModule()
         module.id = str(self.quantity_modules)
         module.info = {'orientation': Orientation.NORTH,
-                       'new_module_type': Alphabet.CORE_COMPONENT}
+                       'module_type': module_type}
         module.orientation = 0
-        module.rgb = [1, 1, 0]
+        module.rgb = self.get_color(module_type)
 
         self.phenotype._body = module
-        self.substrate[(0, 0)] = {'module_type': module,
-                                  'parent_slot': None}
+        # TODO: experiment with evolvable position for the head
+        self.substrate[(0, 0)] = {'module': module,
+                                  'parent_direction': None}
 
-    def place_module(self, module_type):
+    def new_module(self, module_type):
 
         if module_type == Alphabet.BLOCK:
             module = BrickModule()
@@ -124,27 +222,24 @@ class HyperPlasticoding:
             module = TouchSensorModule()
 
         module.info = {}
-        module.info['new_module_type'] = module_type
-        # module.orientation = self.get_angle(module_type,
-        #                                         self.mounting_reference)
+        module.info['module_type'] = module_type
         module.rgb = self.get_color(module_type)
-        if module_type != Alphabet.SENSOR:
-            self.quantity_modules += 1
-            module.id = str(self.quantity_modules)
+
+        return module
 
     def get_angle(self, module_type, parent):
         angle = 0
         if module_type == Alphabet.JOINT_VERTICAL:
-            if parent.info['new_module_type'] is Alphabet.JOINT_VERTICAL:
+            if parent.info['module_type'] is Alphabet.JOINT_VERTICAL:
                 angle = 0
             else:
                 angle = 90
         else:
-            if parent.info['new_module_type'] is Alphabet.JOINT_VERTICAL:
+            if parent.info['module_type'] is Alphabet.JOINT_VERTICAL:
                 angle = 270
         return angle
 
-    def query_body(self, x, y, radius, cppn):
+    def query_body_part(self, x, y, radius, cppn):
 
         x_norm = self.normalize_value(x, -radius, radius)
         y_norm = self.normalize_value(y, -radius, radius)
@@ -163,15 +258,19 @@ class HyperPlasticoding:
 
             is_module = self.get_is_module(outputs[0])
             module_type = self.get_module_type(outputs[1])
-            parent_slot = self.get_parent_slot(outputs[2])
+            parent_direction = self.get_parent_direction(outputs[2])
 
             # ini temporary test!
-            is_module = self.get_is_module(random.uniform(0, 1))
+            is_module = True#self.get_is_module(random.uniform(0, 1))
             module_type = self.get_module_type(random.uniform(0, 1))
-            parent_slot = self.get_parent_slot(random.uniform(0, 1))
+            parent_direction = self.get_parent_direction(random.uniform(0, 1))
             # end temporary test!
 
-            print(is_module, module_type, parent_slot)
+            if is_module:
+                self.substrate[(x, y)] = {'module': self.new_module(module_type),
+                                          'parent_direction': parent_direction}
+
+            print('is_module ', is_module, 'module_type ', module_type, 'parent_direction ', parent_direction)
 
     def get_is_module(self, output):
         is_module = True if output > 0.5 else False
@@ -190,30 +289,33 @@ class HyperPlasticoding:
 
         return module_type
 
-    def get_parent_slot(self, output):
+    def get_parent_direction(self, output):
 
         if 0 <= output < 0.25:
-            parent_slot = Orientation.SOUTH
+            parent_direction = Orientation.SOUTH
         elif 0.25 <= output < 0.5:
-            parent_slot = Orientation.WEST
+            parent_direction = Orientation.WEST
         elif 0.5 <= output < 0.75:
-            parent_slot = Orientation.NORTH
+            parent_direction = Orientation.NORTH
         elif 0.75 <= output <= 1:
-            parent_slot = Orientation.EAST
+            parent_direction = Orientation.EAST
 
-        return parent_slot
+        return parent_direction
 
-    def get_color(self, new_module_type):
+    def get_color(self, module_type):
 
         rgb = []
-        if new_module_type == Alphabet.BLOCK:
+        if module_type == Alphabet.BLOCK:
             rgb = [0, 0, 1]
-        if new_module_type == Alphabet.JOINT_HORIZONTAL:
+        if module_type == Alphabet.JOINT_HORIZONTAL:
             rgb = [1, 0.08, 0.58]
-        if new_module_type == Alphabet.JOINT_VERTICAL:
+        if module_type == Alphabet.JOINT_VERTICAL:
             rgb = [0.7, 0, 0]
-        if new_module_type == Alphabet.SENSOR:
+        if module_type == Alphabet.SENSOR:
             rgb = [0.7, 0.7, 0.7]
+        if module_type == Alphabet.CORE_COMPONENT:
+            rgb = [1, 1, 0]
+
         return rgb
 
     def normalize_value(self, value, min, max):
@@ -225,6 +327,10 @@ class HyperPlasticoding:
         d = abs(x_norm - center) + abs(y_norm - center)
         return d
 
+    def export_genotype(self, filepath):
+        # TODO: export cppn
+        pass
+
 
 class HyperPlasticodingConfig:
     def __init__(self,
@@ -235,7 +341,7 @@ class HyperPlasticodingConfig:
                  robot_id=0,
                  plastic=False,
                  environmental_conditions=['hill'],
-                 substrate_length=3#9
+                 substrate_radius=1#1
                  ):
         self.oscillator_param_min = oscillator_param_min
         self.oscillator_param_max = oscillator_param_max
@@ -244,5 +350,5 @@ class HyperPlasticodingConfig:
         self.robot_id = robot_id
         self.plastic = plastic
         self.environmental_conditions = environmental_conditions
-        self.substrate_length = substrate_length
+        self.substrate_radius = substrate_radius
 
