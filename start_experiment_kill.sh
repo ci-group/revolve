732a29f0d7e668a882d2ac3dff70121c4e27daee
Jie Luo <75667244+onerachel@users.noreply.github.com>
2021-04-15 17:41:36 +0200

    sh to run experiments

---
+++ start_experiment_kill.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#set -e
+#set -x
+
+runs=10
+
+num_terminals=5
+start_port=8000
+final_gen=30
+experiments=("evolution_learning")
+#experiments_path=learner_knn/data/
+manager=experiments/learner_knn/manager_pop.py
+
+while true
+    do
+
+    echo "killing all processes..."
+    kill $(  ps aux | grep 'gzserver' | awk '{print $2}');
+    kill $(  ps aux | grep 'revolve.py' | awk '{print $2}');
+
+    echo "restarting all processes..."
+
+    to_do=()
+
+    for i in $(seq $runs)
+    do
+        run=$(($i))
+
+        for experiment in "${experiments[@]}"
+        do
+
+         echo ""
+         file="${experiment}_${run}.log";
+
+         #check if experiments status
+         if [[ -f "$file" ]]; then
+
+            value=$(grep "Exported snapshot" $file|tail -n1|sed -E "s/\[(.*),.*Exported snapshot ([0-9]+).*/\2/g");
+            echo $file;
+            echo $value;
+            if [ "$value" != "$final_gen" ]; then
+              to_do+=("${experiment}_${run}")
+             fi
+          else
+             echo "$file"
+             echo "None";
+             to_do+=("${experiment}_${run}")
+          fi
+
+        done
+    done
+
+
+    # selects num_terminals unfinished experiments to spawn
+    to_do=(${to_do[@]:0:$num_terminals})
+    for experiment in "${to_do[@]}"
+    do
+
+         echo ""
+         screen -d -m -S "${experiment}" -L -Logfile "${experiment}.log" nice -n19 ./revolve.sh --manager $manager --experiment-name "${experiment}" --evaluation-time 50 --n-cores 4 --port-start $start_port
+         start_port=$((${start_port}+10))
+    done
+
+    sleep 1800s;
+
+done
+
+
+# killall screen
+# screen -r naaameee
+# screen -list
