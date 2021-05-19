1e64a5c9d0147cbe3d2429b3e02234d4b8db736b
Matteo De Carlo <matteo.dek@gmail.com>
2021-05-19 18:28:15 +0200

    Fixed time unit tests and class
    1 sec equals 1e9 nsec, not 10e9 nsec

--- test_py/util/test_time.py
+++ test_py/util/test_time.py
@@ -13,25 +13,25 @@ class TestTime(unittest.TestCase):
     def test_create(self):
         a = Time(0, -10)
         self.assertEqual(a.sec, -1)
-        self.assertEqual(a.nsec, 10e9 - 10)
+        self.assertEqual(a.nsec, 1e9 - 10)
 
     def test_float(self):
-        a = Time(1, 5 * 10e8)
+        a = Time(1, 5e8)
         self.assertAlmostEqual(1.5, float(a))
 
     def test_add(self):
-        a = Time(5, 5*10e8)
+        a = Time(5, 5e8)
         b = a + a
         self.assertEqual(b.sec, 11)
         self.assertEqual(b.nsec, 0)
 
         b = a + 1.1
         self.assertEqual(b.sec, 6)
-        self.assertEqual(b.nsec, 6 * 10e8)
+        self.assertEqual(b.nsec, 6 * 1e8)
 
         b = 1.1 + a
         self.assertEqual(b.sec, 6)
-        self.assertEqual(b.nsec, 6 * 10e8)
+        self.assertEqual(b.nsec, 6 * 1e8)
 
         a += a
         self.assertEqual(a.sec, 11)
@@ -39,39 +39,39 @@ class TestTime(unittest.TestCase):
 
         a += 1.1
         self.assertEqual(a.sec, 12)
-        self.assertEqual(a.nsec, 10e8)
+        self.assertEqual(a.nsec, 1e8)
 
     def test_subtract(self):
-        a = Time(5, 5*10e8)
-        b = a - Time(4, 6 * 10e8)
+        a = Time(5, 5e8)
+        b = a - Time(4, 6 * 1e8)
         self.assertEqual(b.sec, 0)
-        self.assertEqual(b.nsec, 9*10e8)
+        self.assertEqual(b.nsec, 9e8)
 
         b = a - 1.1
         self.assertEqual(b.sec, 4)
-        self.assertEqual(b.nsec, 4*10e8)
+        self.assertEqual(b.nsec, 4e8)
 
         b = 9.9 - a
         self.assertEqual(b.sec, 4)
-        self.assertEqual(b.nsec, 4*10e8)
+        self.assertEqual(b.nsec, 4e8)
 
         a -= a
         self.assertEqual(a.sec, 0)
         self.assertEqual(a.nsec, 0)
 
-        a -= Time(2, 10e8)
+        a -= Time(2, 1e8)
         self.assertEqual(a.sec, -3)
-        self.assertEqual(a.nsec, 9*10e8)
+        self.assertEqual(a.nsec, 9e8)
 
         a += 1.1
         self.assertEqual(a.sec, -1)
         self.assertEqual(a.nsec, 0)
 
     def test_eq(self):
-        a = Time(5, 5*10e8)
+        a = Time(5, 5e8)
         self.assertEqual(a, 5.5)
-        self.assertEqual(a, Time(5, 5*10e8))
-        self.assertNotEqual(a, Time(5, 6*10e8))
+        self.assertEqual(a, Time(5, 5e8))
+        self.assertNotEqual(a, Time(5, 6e8))
 
     def test_cmp(self):
         a = Time(dbl=1.0)
