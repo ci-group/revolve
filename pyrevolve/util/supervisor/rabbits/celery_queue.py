ee4632d26375fc2cab89f39a8b54da7f3149b6d9
Matteo De Carlo <matteo.dek@gmail.com>
2021-06-23 12:02:19 +0200

    Fixed isaac import for people that don't have isaac

--- pyrevolve/util/supervisor/rabbits/celery_queue.py
+++ pyrevolve/util/supervisor/rabbits/celery_queue.py
@@ -19,13 +19,13 @@ from pyrevolve.tol.manage.measures import BehaviouralMeasurements
 from pyrevolve.util.supervisor.rabbits import PostgreSQLDatabase
 from pyrevolve.util.supervisor.rabbits import RobotState
 from pyrevolve.util import Time
-import manage_isaac
 
 app = celery.Celery('CeleryQueue', backend='rpc://', broker='pyamqp://guest@localhost//')
 
 
 @app.task
 def evaluate_robot(robot_sdf: AnyStr, life_timeout: float):
+    import manage_isaac
     manage_isaac.simulator(robot_sdf, life_timeout)
     #raise NotImplementedError("Evaluating a robot is implemented in C++, inside gazebo")
 
