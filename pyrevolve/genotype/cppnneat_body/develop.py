c734a45c3beac9c690794cd38ae82dbb8b44035d
Aart Stuurman <aart@astuurman.com>
2021-07-23 14:49:57 +0200

    Implement body generation algorithm, except for cppn querying

--- pyrevolve/genotype/cppnneat_body/develop.py
+++ pyrevolve/genotype/cppnneat_body/develop.py
@@ -11,10 +11,16 @@ from pyrevolve.revolve_bot.revolve_module import (
     RevolveModule,
 )
 
+"""
+Not using a library for vector because it's super simple with int and I don't feel like using numpy at this moment
+"""
+
 
 @dataclass
 class _Module:
     position: Tuple[int, int, int]
+    forward: Tuple[int, int, int]
+    up: Tuple[int, int, int]
     chain_length: int
     module_reference: CoreModule
 
@@ -29,9 +35,11 @@ def cppnneat_body_develop(
     core_module = CoreModule()
     core_module.id = "core"
     core_module.rgb = [1, 1, 0]
-    core_module.orientation = 0
+    core_module.orientation = 1
 
-    to_explore.put(_Module((0, 0, 0), 0, core_module))
+    to_explore.put(
+        _Module((0, 0, 0), (0, -1, 0), (0, 0, 1), 0, core_module)
+    )  # forward is always slot 1
     part_count = 1
 
     while not to_explore.empty():
@@ -65,21 +73,102 @@ def _get_child_type(
 
 
 def _add_child(module: _Module, child_index: int) -> Optional[_Module]:
-    child_type, orientation = _get_child_type(module.position, module.chain_length)
+    forward = get_new_forward(module.forward, module.up, child_index)
+    position = _add(module.position, forward)
+    chain_length = module.chain_length + 1
+
+    child_type, orientation = _get_child_type(position, chain_length)
     if child_type == None:
         return None
+
     child = child_type()
+    module.module_reference.children[child_index] = child
     child.id = module.module_reference.id + "_" + str(child_index)
-    child.orientation = orientation
+    child.orientation = orientation * 90
+
+    # coloring
     if child_type == BrickModule:
         child.rgb = [1, 0, 0]
     elif child_type == ActiveHingeModule:
         child.rgb = [0, 1, 0]
-    else:
-        # Should actually never arrive here but just checking module type to be sure
+    else:  # Should actually never arrive here but just checking module type to be sure
         raise RuntimeError
 
-    module.module_reference.children[child_index] = child
-    return _Module((0, 0, 0), 0, child)  # TODO
+    up = _get_new_up(module.up, forward, orientation)
+    return _Module(
+        position,
+        forward,
+        up,
+        chain_length,
+        child,
+    )
 
+    # TODO check for self collision?
     # revolve_bot -> update_subtrate throws on collision (raise for intersection True)
+
+
+def get_new_forward(
+    parent_forward: Tuple[int, int, int], parent_up: Tuple[int, int, int], slot: int
+) -> Tuple[int, int, int]:
+    rotation: int
+    if slot == 0:
+        rotation = 2
+    elif slot == 1:
+        rotation = 0
+    elif slot == 2:
+        rotation = 3
+    else:
+        rotation = 1
+
+    return _rotate(parent_forward, parent_up, rotation)
+
+
+def _get_new_up(
+    parent_up: Tuple[int, int, int], new_forward: Tuple[int, int, int], orientation: int
+) -> Tuple[int, int, int]:
+    return _rotate(parent_up, new_forward, orientation)
+
+
+def _add(a: Tuple[int, int, int], b: Tuple[int, int, int]) -> Tuple[int, int, int]:
+    return tuple(map(sum, zip(a, b)))
+
+
+def _timesscalar(a: Tuple[int, int, int], scalar: int) -> Tuple[int, int, int]:
+    return (a[0] * scalar, a[1] * scalar, a[2] * scalar)
+
+
+def _cross(a: Tuple[int, int, int], b: Tuple[int, int, int]) -> Tuple[int, int, int]:
+    return (
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0],
+    )
+
+
+def _dot(a: Tuple[int, int, int], b: Tuple[int, int, int]) -> int:
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+# rotates a around b. angle from [0,1,2,3]. 90 degrees each
+def _rotate(
+    a: Tuple[int, int, int], b: Tuple[int, int, int], angle: int
+) -> Tuple[int, int, int]:
+    cosangle: int
+    sinangle: int
+    if angle == 0:
+        cosangle = 1
+        sinangle = 0
+    elif angle == 1:
+        cosangle = 0
+        sinangle = 1
+    elif angle == 2:
+        cosangle = -1
+        sinangle = 0
+    else:
+        cosangle = 0
+        sinangle = -1
+
+    return _add(
+        _add(_timesscalar(a, cosangle), _timesscalar(_cross(b, a), sinangle)),
+        _timesscalar(b, _dot(b, a) * (1 - cosangle)),
+    )
