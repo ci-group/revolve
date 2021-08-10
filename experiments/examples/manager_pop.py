68f6cf7d747d6df9ae7e9b72fe9e00982db4ec24
Aart Stuurman <aart@astuurman.com>
2021-08-10 15:07:19 +0200

    Repair examples/manager_pop.py

--- experiments/examples/manager_pop.py
+++ experiments/examples/manager_pop.py
@@ -2,20 +2,25 @@
 import asyncio
 
 from pyrevolve import parser
+from pyrevolve.custom_logging.logger import logger
 from pyrevolve.evolution import fitness
+from pyrevolve.evolution.population.population import Population
+from pyrevolve.evolution.population.population_config import PopulationConfig
+from pyrevolve.evolution.population.population_management import (
+    steady_state_population_management,
+)
 from pyrevolve.evolution.selection import multiple_selection, tournament_selection
-from pyrevolve.evolution.population import Population, PopulationConfig
-from pyrevolve.evolution.pop_management.steady_state import steady_state_population_management
 from pyrevolve.experiment_management import ExperimentManagement
+from pyrevolve.genotype.plasticoding import PlasticodingConfig
 from pyrevolve.genotype.plasticoding.crossover.crossover import CrossoverConfig
-from pyrevolve.genotype.plasticoding.crossover.standard_crossover import standard_crossover
+from pyrevolve.genotype.plasticoding.crossover.standard_crossover import (
+    standard_crossover,
+)
 from pyrevolve.genotype.plasticoding.initialization import random_initialization
 from pyrevolve.genotype.plasticoding.mutation.mutation import MutationConfig
 from pyrevolve.genotype.plasticoding.mutation.standard_mutation import standard_mutation
-from pyrevolve.genotype.plasticoding import PlasticodingConfig
 from pyrevolve.util.supervisor.analyzer_queue import AnalyzerQueue
 from pyrevolve.util.supervisor.simulator_queue import SimulatorQueue
-from pyrevolve.custom_logging.logger import logger
 
 
 async def run():
@@ -33,7 +38,7 @@ async def run():
         allow_vertical_brick=True,
         use_movement_commands=True,
         use_rotation_commands=False,
-        use_movement_stack=True
+        use_movement_stack=True,
     )
 
     mutation_conf = MutationConfig(
@@ -49,15 +54,23 @@ async def run():
     # Parse command line / file input arguments
     settings = parser.parse_args()
     experiment_management = ExperimentManagement(settings)
-    do_recovery = settings.recovery_enabled and not experiment_management.experiment_is_new()
+    do_recovery = (
+        settings.recovery_enabled and not experiment_management.experiment_is_new()
+    )
 
-    logger.info('Activated run '+settings.run+' of experiment '+settings.experiment_name)
+    logger.info(
+        "Activated run " + settings.run + " of experiment " + settings.experiment_name
+    )
 
     if do_recovery:
-        gen_num, has_offspring, next_robot_id = experiment_management.read_recovery_state(population_size, offspring_size)
+        (
+            gen_num,
+            has_offspring,
+            next_robot_id,
+        ) = experiment_management.read_recovery_state(population_size, offspring_size)
 
-        if gen_num == num_generations-1:
-            logger.info('Experiment is already complete.')
+        if gen_num == num_generations - 1:
+            logger.info("Experiment is already complete.")
             return
     else:
         gen_num = 0
@@ -73,7 +86,9 @@ async def run():
         crossover_operator=standard_crossover,
         crossover_conf=crossover_conf,
         selection=lambda individuals: tournament_selection(individuals, 2),
-        parent_selection=lambda individuals: multiple_selection(individuals, 2, tournament_selection),
+        parent_selection=lambda individuals: multiple_selection(
+            individuals, 2, tournament_selection
+        ),
         population_management=steady_state_population_management,
         population_management_selector=tournament_selection,
         evaluation_time=settings.evaluation_time,
@@ -82,29 +97,39 @@ async def run():
         experiment_management=experiment_management,
     )
 
-    n_cores =  settings.n_cores
+    n_cores = settings.n_cores
 
     settings = parser.parse_args()
     simulator_queue = SimulatorQueue(n_cores, settings, settings.port_start)
     await simulator_queue.start()
 
-    analyzer_queue = AnalyzerQueue(1, settings, settings.port_start+n_cores)
+    analyzer_queue = AnalyzerQueue(1, settings, settings.port_start + n_cores)
     await analyzer_queue.start()
 
-    population = Population(population_conf, simulator_queue, analyzer_queue, next_robot_id)
+    population = Population(
+        population_conf, simulator_queue, analyzer_queue, next_robot_id
+    )
 
     if do_recovery:
         # loading a previous state of the experiment
         await population.load_snapshot(gen_num)
         if gen_num >= 0:
-            logger.info('Recovered snapshot '+str(gen_num)+', pop with ' + str(len(population.individuals))+' individuals')
+            logger.info(
+                "Recovered snapshot "
+                + str(gen_num)
+                + ", pop with "
+                + str(len(population.individuals))
+                + " individuals"
+            )
         if has_offspring:
-            individuals = await population.load_offspring(gen_num, population_size, offspring_size, next_robot_id)
+            individuals = await population.load_offspring(
+                gen_num, population_size, offspring_size, next_robot_id
+            )
             gen_num += 1
-            logger.info('Recovered unfinished offspring '+str(gen_num))
+            logger.info("Recovered unfinished offspring " + str(gen_num))
 
             if gen_num == 0:
-                await population.init_pop(individuals)
+                await population.initialize(individuals)
             else:
                 population = await population.next_gen(gen_num, individuals)
 
@@ -112,10 +137,10 @@ async def run():
     else:
         # starting a new experiment
         experiment_management.create_exp_folders()
-        await population.init_pop()
+        await population.initialize()
         experiment_management.export_snapshots(population.individuals, gen_num)
 
-    while gen_num < num_generations-1:
+    while gen_num < num_generations - 1:
         gen_num += 1
         population = await population.next_gen(gen_num)
         experiment_management.export_snapshots(population.individuals, gen_num)
