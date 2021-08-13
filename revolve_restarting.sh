f2ab83eb02f8e668bd172f5be22666f1a748d7ac
Aart Stuurman <aart@astuurman.com>
2021-08-13 10:16:23 +0200

    Add script that restarts revolve after crashing.

---
+++ revolve_restarting.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Running command:"
+echo "./revolve.py $@"
+until ./revolve.py "$@"
+do
+    "Revolve crashed. This can be normal behaviour, because of third party software out of our control. Trying again in a few seconds.."
+    sleep 2
+done
