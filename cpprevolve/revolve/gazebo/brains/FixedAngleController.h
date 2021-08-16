35ed7544ae02d8172e75d2db2b7aa79e73870776
Aart Stuurman <aart@astuurman.com>
2021-08-16 17:18:46 +0200

    Removed ActuatorWrapper

--- cpprevolve/revolve/gazebo/brains/FixedAngleController.h
+++ cpprevolve/revolve/gazebo/brains/FixedAngleController.h
@@ -5,37 +5,34 @@
 #ifndef GAZEBO_REVOLVE_FIXEDANGLECONTROLLER_H
 #define GAZEBO_REVOLVE_FIXEDANGLECONTROLLER_H
 
-
 #include <revolve/brains/controller/actuators/Actuator.h>
 #include <revolve/brains/controller/sensors/Sensor.h>
 #include <revolve/brains/controller/FixedAngleController.h>
-#include <revolve/gazebo/motors/ActuatorWrapper.h>
 #include "Brain.h"
 
 namespace revolve
 {
-namespace gazebo
-{
-
-class FixedAngleController: public Brain, private revolve::FixedAngleController
-{
-public:
-    explicit FixedAngleController(double angle)
-        : Brain()
-        , revolve::FixedAngleController(angle)
-    {}
-
-    void Update(const std::vector<MotorPtr> &motors,
-                const std::vector<SensorPtr> &sensors,
-                const double time,
-                const double step) override
+    namespace gazebo
     {
-        revolve::FixedAngleController::update(motors, sensors, time, step);
+
+        class FixedAngleController : public Brain, private revolve::FixedAngleController
+        {
+        public:
+            explicit FixedAngleController(double angle)
+                : Brain(), revolve::FixedAngleController(angle)
+            {
+            }
+
+            void Update(const std::vector<MotorPtr> &motors,
+                        const std::vector<SensorPtr> &sensors,
+                        const double time,
+                        const double step) override
+            {
+                revolve::FixedAngleController::update(motors, sensors, time, step);
+            }
+        };
+
     }
-};
-
 }
-}
-
 
 #endif //GAZEBO_REVOLVE_FIXEDANGLECONTROLLER_H
