b3bcc005e740bd39e4df4c245ad897a693d74b08
Aart Stuurman <aart@astuurman.com>
2021-08-16 17:27:49 +0200

    fixed typo

--- pyrevolve/evolution/population/population_management.py
+++ pyrevolve/evolution/population/population_management.py
@@ -2,7 +2,7 @@ from pyrevolve.evolution.selection import multiple_selection
 
 
 def steady_state_population_management(old_individuals, new_individuals, selector):
-    # Stead state population
+    # Steady state population
     # total size of population is the sum of all the species individuals.
     pop_size = len(old_individuals)
     selection_pool = old_individuals + new_individuals
@@ -11,6 +11,5 @@ def steady_state_population_management(old_individuals, new_individuals, selecto
 
 
 def generational_population_management(old_individuals, new_individuals):
-    assert (len(old_individuals) == len(new_individuals))
+    assert len(old_individuals) == len(new_individuals)
     return new_individuals
-
