3b179be6e156a1a8c0606b4cc1fddc6fa1925f3b
Aart Stuurman <aart@astuurman.com>
2021-08-31 13:19:57 +0200

    Fix irrelevant bug in revolve restarting

--- revolve_restarting.sh
+++ revolve_restarting.sh
@@ -4,6 +4,6 @@ echo "Running command:"
 echo "./revolve.py $@"
 until ./revolve.py "$@"
 do
-    "Revolve crashed. This can be normal behaviour, because of third party software out of our control. Trying again in a few seconds.."
+    echo "Revolve crashed. This can be normal behaviour, because of third party software out of our control. Trying again in a few seconds.."
     sleep 2
 done
