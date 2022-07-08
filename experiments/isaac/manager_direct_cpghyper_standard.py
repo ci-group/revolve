6e136770ff83228f0afe11338f524a939752c3ec
Matteo De Carlo <matteo.dek@covolunablu.org>
2022-07-08 17:12:54 +0200

    Standard evolution test, to verify that everything works as expected

---
+++ experiments/isaac/manager_direct_cpghyper_standard.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import os
+from typing import List
+
+import math
+from isaacgym import gymapi
+
+from pyrevolve import parser
+from pyrevolve.custom_logging.logger import logger
+from pyrevolve.evolution import fitness
+from pyrevolve.evolution.population.population import Population
+from pyrevolve.evolution.population.population_config import PopulationConfig
+from pyrevolve.evolution.population.population_management import generational_population_management, steady_state_population_management
+from pyrevolve.evolution.selection import tournament_selection, multiple_selection
+from pyrevolve.experiment_management import ExperimentManagement
+from pyrevolve.genotype.direct_tree.direct_tree_genotype import DirectTreeGenotypeConfig
+from pyrevolve.genotype.neat_brain_genome.neat_brain_genome import NeatBrainGenomeConfig, BrainType
+from pyrevolve.genotype.neat_brain_genome.crossover import NEATCrossoverConf
+from pyrevolve.genotype.tree_body_hyperneat_brain import DirectTreeCPGHyperNEATGenotypeConfig, \
+    DirectTreeCPGHyperNEATGenotype
+from pyrevolve.genotype.tree_body_hyperneat_brain.crossover import standard_crossover
+from pyrevolve.genotype.tree_body_hyperneat_brain.mutation import standard_mutation
+from pyrevolve.util.supervisor.rabbits import GazeboCeleryWorkerSupervisor, PostgreSQLDatabase, RobotEvaluation, \
+    RobotState, Robot as DBRobot
+from pyrevolve.util.supervisor.rabbits.celery_queue import CeleryPopulationQueue
+
+INTERNAL_WORKERS = False
+PROGENITOR = False
+
+
+async def run():
+    """
+    The main coroutine, which is started below.
+    """
+
+    # experiment params #
+    num_generations = 100
+    population_size = 100
+    offspring_size = 50
+
+    morph_single_mutation_prob = 0.2
+    morph_no_single_mutation_prob = 1 - morph_single_mutation_prob  # 0.8
+    morph_no_all_mutation_prob = morph_no_single_mutation_prob ** 4  # 0.4096
+    morph_at_least_one_mutation_prob = 1 - morph_no_all_mutation_prob  # 0.5904
+
+    brain_single_mutation_prob = 0.5
+
+    tree_genotype_conf: DirectTreeGenotypeConfig = DirectTreeGenotypeConfig(
+        max_parts=25,
+        min_parts=5,
+        max_oscillation=5,
+        init_n_parts_mu=10,
+        init_n_parts_sigma=4,
+        init_prob_no_child=0.1,
+        init_prob_child_block=0.4,
+        init_prob_child_active_joint=0.5,
+        mutation_p_duplicate_subtree=morph_single_mutation_prob,
+        mutation_p_delete_subtree=morph_single_mutation_prob,
+        mutation_p_generate_subtree=morph_single_mutation_prob,
+        mutation_p_swap_subtree=morph_single_mutation_prob,
+        mutation_p_mutate_oscillators=brain_single_mutation_prob,
+        mutation_p_mutate_oscillator=0,
+        mutate_oscillator_amplitude_sigma=0.3,
+        mutate_oscillator_period_sigma=0.3,
+        mutate_oscillator_phase_sigma=0.3,
+    )
+
+    neat_conf: NeatBrainGenomeConfig = NeatBrainGenomeConfig(
+        brain_type=BrainType.CPG,
+        random_seed=None,
+        apply_mutation_constraints=False,
+    )
+
+    neat_crossover_conf: NEATCrossoverConf = NEATCrossoverConf(
+        apply_constraints=False,
+    )
+
+    genotype_conf: DirectTreeCPGHyperNEATGenotypeConfig = DirectTreeCPGHyperNEATGenotypeConfig(
+        direct_tree_conf=tree_genotype_conf,
+        neat_conf=neat_conf,
+        neat_crossover_conf=neat_crossover_conf,
+        number_of_brains=1,
+    )
+
+    def genotype_test_fun(candidate_genotype: DirectTreeCPGHyperNEATGenotype) -> bool:
+        for brain_gen in candidate_genotype._brain_genomes:
+            if brain_gen._neat_genome.FailsConstraints(neat_conf.multineat_params):
+                return False
+        return True
+
+    # Parse command line / file input arguments
+    args = parser.parse_args()
+    experiment_management = ExperimentManagement(args)
+    has_offspring = False
+    do_recovery = args.recovery_enabled and not experiment_management.experiment_is_new()
+
+    logger.info(f'Activated run {args.run} of experiment {args.experiment_name}')
+
+    if do_recovery:
+        gen_num, has_offspring, next_robot_id, next_species_id = \
+            experiment_management.read_recovery_state(population_size, offspring_size, species=False)
+
+        if gen_num == num_generations - 1:
+            logger.info('Experiment is already complete.')
+            return
+    else:
+        gen_num = 0
+        next_robot_id = 1
+
+    if gen_num < 0:
+        logger.info('Experiment continuing from first generation')
+        gen_num = 0
+
+    if next_robot_id < 0:
+        next_robot_id = 1
+
+    population_conf = PopulationConfig(
+        population_size=population_size,
+        genotype_constructor=lambda conf, _id: DirectTreeCPGHyperNEATGenotype(conf, _id, random_init_body=True),
+        genotype_conf=genotype_conf,
+        fitness_function=fitness.displacement_velocity,
+        objective_functions=None,
+        mutation_operator=lambda genotype, gen_conf: standard_mutation(genotype, gen_conf),
+        mutation_conf=genotype_conf,
+        crossover_operator=lambda parents, gen_conf, _: standard_crossover(parents, gen_conf),
+        crossover_conf=None,
+        genotype_test=genotype_test_fun,
+        selection=lambda pop: tournament_selection(pop, k=2),
+        parent_selection=lambda potential_parents: multiple_selection(potential_parents, 2, tournament_selection),
+        population_management=steady_state_population_management,
+        population_management_selector=tournament_selection,
+        evaluation_time=args.evaluation_time,
+        grace_time=args.grace_time,
+        offspring_size=offspring_size,
+        experiment_name=args.experiment_name,
+        experiment_management=experiment_management,
+    )
+
+    n_cores = args.n_cores
+
+    def worker_crash(process, exit_code) -> None:
+        logger.fatal(f'GazeboCeleryWorker died with code: {exit_code} ({process})')
+
+    # CELERY CONNECTION (includes database connection)
+    # simulator_queue = CeleryQueue(args, args.port_start, dbname='revolve', db_addr='127.0.0.1', use_isaacgym=True)
+    simulator_queue = CeleryPopulationQueue(args, use_isaacgym=True, local_computing=True)
+    await simulator_queue.start(cleanup_database=True)
+
+    # CELERY GAZEBO WORKER
+    celery_workers: List[GazeboCeleryWorkerSupervisor] = []
+    if INTERNAL_WORKERS:
+        for n in range(n_cores):
+            celery_worker = GazeboCeleryWorkerSupervisor(
+                world_file='worlds/plane.celery.world',
+                gui=args.gui,
+                simulator_args=['--verbose'],
+                plugins_dir_path=os.path.join('../heritability', 'build', 'lib'),
+                models_dir_path=os.path.join('../heritability', 'models'),
+                simulator_name=f'GazeboCeleryWorker_{n}',
+                process_terminated_callback=worker_crash,
+            )
+            await celery_worker.launch_simulator(port=args.port_start + n)
+            celery_workers.append(celery_worker)
+
+    # ANALYZER CONNECTION
+    # analyzer_port = args.port_start + (n_cores if INTERNAL_WORKERS else 0)
+    # analyzer_queue = AnalyzerQueue(1, args, port_start=analyzer_port)
+    # await analyzer_queue.start()
+    analyzer_queue = None
+
+    # INITIAL POPULATION OBJECT
+    population = Population(population_conf, simulator_queue, analyzer_queue, next_robot_id)
+
+    if do_recovery:
+        # loading a previous state of the experiment
+        population.load_snapshot(gen_num, multi_development=True)
+
+        # drop unfinished db elements
+        with simulator_queue._db.session() as session:
+            extra_robots = session.query(DBRobot).filter(DBRobot.id >= next_robot_id)
+            robot_ids: List[int] = [r.id for r in extra_robots]
+            if len(robot_ids) > 0:
+                print(f'Dropping unfinished robots (DBIDs={robot_ids})')
+                #
+                #session.query(RobotState).filter(RobotState.evaluation_robot_id.in_(robot_ids)).delete()
+                #session.commit()
+                #session.query(RobotEvaluation).filter(RobotEvaluation.robot_id.in_(robot_ids)).delete()
+                #session.commit()
+                #extra_robots.delete()
+                #session.commit()
+        if gen_num >= 0:
+            logger.info(f'Recovered snapshot {gen_num}, pop with {len(population.individuals)} individuals')
+        if has_offspring:
+            individuals = population.load_offspring(gen_num, population_size, offspring_size, next_robot_id)
+            gen_num += 1
+            logger.info(f'Recovered unfinished offspring {gen_num}')
+
+            if gen_num == 0:
+                assert len(individuals) == 0
+                if PROGENITOR:
+                    await population.initialize_from_single_individual()
+                else:
+                    await population.initialize()
+            else:
+                population = await population.next_generation(gen_num, individuals)
+
+            experiment_management.export_snapshots(population.individuals, gen_num)
+    else:
+        # starting a new experiment
+        experiment_management.create_exp_folders()
+        if PROGENITOR:
+            await population.initialize_from_single_individual()
+        else:
+            await population.initialize()
+        experiment_management.export_snapshots(population.individuals, gen_num)
+
+    while gen_num < num_generations - 1:
+        gen_num += 1
+        population = await population.next_generation(gen_num)
+        experiment_management.export_snapshots(population.individuals, gen_num)
+
+    # CLEANUP
+    for celery_worker in celery_workers:
+        await celery_worker.stop()
+    await simulator_queue.stop()
